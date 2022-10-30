e256abfa9ae0b5d935aea07c8fb4c2655686e6e2
Alec Saunders <alecsaunders90@gmail.com>
2022-10-29 22:45:52 -0600

    Initial Commit

---
+++ Captions/CaptionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptionsApp.swift
+//  Captions
+//
+//  Created by Saunders, Alec on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CaptionsApp: App {
+    var body: some Scene {
+        DocumentGroup(newDocument: CaptionsDocument()) { file in
+            ContentView(document: file.$document)
+        }
+    }
+}
---
+++ Captions/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Captions
+//
+//  Created by Saunders, Alec on 10/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Binding var document: CaptionsDocument
+
+    var body: some View {
+        TextEditor(text: $document.text)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(document: .constant(CaptionsDocument()))
+    }
+}

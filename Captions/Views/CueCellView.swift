a06922e8d88e7f627455aa2b4bc7fd5a9ad5caac
Alec Saunders <alecsaunders90@gmail.com>
2022-11-01 22:03:12 -0600

    Add a delete button to delete a row

--- Captions/Views/CueCellView.swift
+++ Captions/Views/CueCellView.swift
@@ -13,27 +13,50 @@ struct CueView: View {
     @Binding var cue: Cue
     @Binding var highlighted: Cue?
     @State var shiftControlOpts = ShiftControlOptions()
+    @State var hoveringOverClearButton = false
 
 
     var body: some View {
         VStack {
-            VStack {
-                CueHeaderView(cue: $cue)
-                CueTimingsView(cue: $cue, highlighted: $highlighted, shiftControlOpts: $shiftControlOpts)
-                CueTextView(cue: $cue)
-            }
-                .onHover { hovering in
-                    if hovering {
-                        highlighted = cue
-                        shiftControlOpts.resetOptions()
-                    } else {
-                        highlighted = nil
+            ZStack {
+                VStack {
+                    CueHeaderView(cue: $cue)
+                    CueTimingsView(cue: $cue, highlighted: $highlighted, shiftControlOpts: $shiftControlOpts)
+                    CueTextView(cue: $cue)
+                }
+                    .onHover { hovering in
+                        if hovering {
+                            highlighted = cue
+                            shiftControlOpts.resetOptions()
+                        } else {
+                            highlighted = nil
+                        }
+                    }
+                    .padding(5)
+                    .background(cue.id == highlighted?.id ? .secondary.opacity(0.125) : Color.clear)
+                    .cornerRadius(8)
+                    .padding(.leading, 8)
+                if highlighted?.id == cue.id {
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Button {
+                                captions.removeCue(withId: cue.identifier)
+                            } label: {
+                                Image(systemName: "multiply.circle")
+                            }
+                                .buttonStyle(.borderless)
+                                .padding(5)
+                                .onHover { hovering in
+                                    hoveringOverClearButton = hovering
+                                }
+                                .tint(hoveringOverClearButton ? .red : .primary)
+                            Spacer()
+                        }
                     }
                 }
-                .padding(5)
-                .background(cue.id == highlighted?.id ? .secondary.opacity(0.125) : Color.clear)
-                .cornerRadius(8)
-                .padding(.leading, 8)
+            }
+            
             Divider()
         }
         .popover(isPresented: $shiftControlOpts.showShiftPopover, attachmentAnchor: .rect(.rect(CGRect(x: shiftControlOpts.isStart ? 50 : 175, y: 5, width: 0, height: 0))) ) {

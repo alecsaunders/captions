5071304224263dc4f93eba3333dd0e9c76e0ad58
Alec Saunders <alecsaunders90@gmail.com>
2022-11-01 21:04:48 -0600

    Use list instead of scroll view

--- Captions/ContentView.swift
+++ Captions/ContentView.swift
@@ -13,29 +13,26 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack {
-                    ForEach($document.captions.cues) { $cue in
-                        HStack {
-                            CueView(captions: $document.captions, cue: $cue, highlighted: $highlighted)
-                            Spacer()
-                        }
-                        .contextMenu {
-                            Button("Delete row") {
-                                document.captions.removeCue(withId: cue.identifier)
-                            }
+            List {
+                ForEach($document.captions.cues) { $cue in
+                    HStack {
+                        CueView(captions: $document.captions, cue: $cue, highlighted: $highlighted)
+                        Spacer()
+                    }
+                    .contextMenu {
+                        Button("Delete row") {
+                            self.document.captions.cues.removeAll { $0.id == cue.id }
                         }
                     }
                 }
             }
                 .frame(minWidth: 268)
-            
             VStack {
                 Text("Main body")
                     .padding(25)
                 Spacer()
             }
-            
+
         }
     }
 }
